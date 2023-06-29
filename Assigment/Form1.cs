04dc86d0a5bfec0adaafec7ed38f61667e48dc2d
MSI\MSI LAPTOP <dat36226@gmail.com>
2023-06-30 02:56:24 +0700

    Update code

--- Assigment/Form1.cs
+++ Assigment/Form1.cs
@@ -10,6 +10,8 @@ namespace Assigment
     public partial class Form1 : Form
     {
         MjpegDecoder mjp;
+        Bitmap capturedImage;
+
 
         public Form1()
         {
@@ -25,27 +27,51 @@ namespace Assigment
 
         private void button1_Click(object sender, EventArgs e)
         {
-            mjp.ParseStream(new Uri(" http://192.168.138.165:4747/video?640x480"));
+            mjp.ParseStream(new Uri(" http://192.168.27.100:4747/video?640x480"));
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (pictureBox1.Image != null)
-            {
-                // Lưu ảnh từ PictureBox vào tệp tin
-                SaveFileDialog saveFileDialog = new SaveFileDialog();
-                saveFileDialog.Filter = "Image Files (*.jpg; *.jpeg; *.png)|*.jpg; *.jpeg; *.png";
-                if (saveFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    pictureBox1.Image.Save(saveFileDialog.FileName);
-                    MessageBox.Show("Đã lưu ảnh thành công!");
-                }
-            }
-            else
-            {
-                MessageBox.Show("Không có khung hình để chụp!");
-            }
+            // Ghi lại và xuất số 
+            capturedImage = (Bitmap)pictureBox1.Image.Clone();
+            int randomValue = new Random().Next();
+            ShowCapturedImage(capturedImage, randomValue);
+            
+
+            //Save picture
+            //if (pictureBox1.Image != null)
+            //{
+            //    // Lưu ảnh từ PictureBox vào tệp tin
+            //    SaveFileDialog saveFileDialog = new SaveFileDialog();
+            //    saveFileDialog.Filter = "Image Files (*.jpg; *.jpeg; *.png)|*.jpg; *.jpeg; *.png";
+            //    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            //    {
+            //        pictureBox1.Image.Save(saveFileDialog.FileName);
+
+                  
+            //        MessageBox.Show("Đã chụp ảnh thành công!");
+
+            //    }
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Không có khung hình để chụp!");
+            //}
         }
+
+        //Hàm xuất số ngẫu nhiên đi kèm hình
+        private void ShowCapturedImage(Bitmap image, int randomValue)
+        {
+            // Tạo một instance của ShowForm
+            ShowForm showForm = new ShowForm();
+
+            // Hiển thị ảnh đã chụp và giá trị ngẫu nhiên trên ShowForm
+            showForm.ShowImage(image, randomValue);
+
+            // Hiển thị form ShowForm
+            showForm.Show();
+        }
+
     }
 }
 
---
+++ Assigment/ShowForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assigment
+{
+    public partial class ShowForm : Form
+    {
+        public ShowForm()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowImage(Bitmap image, int randomValue)
+        {
+            pictureBox1.Image = image;
+            textBox1.Text = randomValue.ToString();
+        }
+    }
+}
